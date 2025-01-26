f2e885ca10fd28227b8c57aadee5b3bd595676c2
kmrifat <kmrifat@gmail.com>
2025-01-27 03:07:09 +0600

    experiment on going

--- pages/career/index.vue
+++ pages/career/index.vue
@@ -3,89 +3,97 @@
 </script>
 
 <template>
-  <div class="container">
-    <div class="row my-5">
-      <div class="col-md-4">
-        <h3>Current Available Positions at Quiety</h3>
-      </div>
+  <div class="bg-career">
+    <div class="container">
+      <div class="row my-5">
+        <div class="col-md-4">
+          <h3>Current Available Positions at Quiety</h3>
+        </div>
 
-      <div class="col-md-8">
-        <p>Phosfluorescently disintermediate revolutionary paradigms before enabled interfaces. Dynamically transition
-          skills vis-a-vis virtual customer service via impactful partnerships with technically sound paradigms with
-          cutting-edge initiatives.</p>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-6">
-        <div class="card mb-4">
-          <div class="card-body">
-            <p>Remote - Full Time</p>
-            <h4>Jr Frontend Developer</h4>
-            <p>Work from home | 20,000 - 30,000</p>
-            <button class="btn btn-primary">Apply Now</button>
-          </div>
+        <div class="col-md-8">
+          <p>Phosfluorescently disintermediate revolutionary paradigms before enabled interfaces. Dynamically transition
+            skills vis-a-vis virtual customer service via impactful partnerships with technically sound paradigms with
+            cutting-edge initiatives.</p>
         </div>
       </div>
 
-      <div class="col-md-6">
-        <div class="card mb-4">
-          <div class="card-body">
-            <p>Remote - Full Time</p>
-            <h4>Jr Backend Developer</h4>
-            <p>Work from home | 20,000 - 30,000</p>
-            <button class="btn btn-primary">Apply Now</button>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card mb-4">
+            <div class="card-body">
+              <p>Remote - Full Time</p>
+              <h4>Jr Frontend Developer</h4>
+              <p>Work from home | 20,000 - 30,000</p>
+              <button class="btn btn-primary">Apply Now</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md-6">
-        <div class="card mb-4">
-          <div class="card-body">
-            <p>Remote - Full Time</p>
-            <h4>Jr Frontend Developer</h4>
-            <p>Work from home | 20,000 - 30,000</p>
-            <button class="btn btn-primary">Apply Now</button>
+        <div class="col-md-6">
+          <div class="card mb-4">
+            <div class="card-body">
+              <p>Remote - Full Time</p>
+              <h4>Jr Backend Developer</h4>
+              <p>Work from home | 20,000 - 30,000</p>
+              <button class="btn btn-primary">Apply Now</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md-6">
-        <div class="card mb-4">
-          <div class="card-body">
-            <p>Remote - Full Time</p>
-            <h4>Jr Backend Developer</h4>
-            <p>Work from home | 20,000 - 30,000</p>
-            <button class="btn btn-primary">Apply Now</button>
+        <div class="col-md-6">
+          <div class="card mb-4">
+            <div class="card-body">
+              <p>Remote - Full Time</p>
+              <h4>Jr Frontend Developer</h4>
+              <p>Work from home | 20,000 - 30,000</p>
+              <button class="btn btn-primary">Apply Now</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md-6">
-        <div class="card mb-4">
-          <div class="card-body">
-            <p>Remote - Full Time</p>
-            <h4>Jr Frontend Developer</h4>
-            <p>Work from home | 20,000 - 30,000</p>
-            <button class="btn btn-primary">Apply Now</button>
+        <div class="col-md-6">
+          <div class="card mb-4">
+            <div class="card-body">
+              <p>Remote - Full Time</p>
+              <h4>Jr Backend Developer</h4>
+              <p>Work from home | 20,000 - 30,000</p>
+              <button class="btn btn-primary">Apply Now</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md-6">
-        <div class="card mb-4">
-          <div class="card-body">
-            <p>Remote - Full Time</p>
-            <h4>Jr Backend Developer</h4>
-            <p>Work from home | 20,000 - 30,000</p>
-            <button class="btn btn-primary">Apply Now</button>
+        <div class="col-md-6">
+          <div class="card mb-4">
+            <div class="card-body">
+              <p>Remote - Full Time</p>
+              <h4>Jr Frontend Developer</h4>
+              <p>Work from home | 20,000 - 30,000</p>
+              <button class="btn btn-primary">Apply Now</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="card mb-4">
+            <div class="card-body">
+              <p>Remote - Full Time</p>
+              <h4>Jr Backend Developer</h4>
+              <p>Work from home | 20,000 - 30,000</p>
+              <button class="btn btn-primary">Apply Now</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
 </template>
 
 <style scoped>
-
+.bg-career {
+  background-image: url('/assets/img/bg-vector/vector-2.jpg');
+  background-repeat: no-repeat;
+  background-position: bottom;
+  background-size: cover;
+}
 </style>
